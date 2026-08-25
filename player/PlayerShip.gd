dd9fc02c841a7eb1b6eb2ea4b81875102b4e1277
richardjankovich6 <jedidallin@gmail.com>
2026-08-25 15:40:26 -0600

    add break button add acceleration modifier

--- player/PlayerShip.gd
+++ player/PlayerShip.gd
@@ -1,8 +1,11 @@
 extends RigidBody3D
 
 var pending_impules : Vector3 = Vector3.ZERO
-var rot_x = 0
-var rot_y = 0
+
+var acceleration_rate : Vector3 = Vector3(1.0, 0.5, 0.4)
+
+var screen_x_rotate : float = 0.0
+var screen_y_rotate : float = 0.0
 var LOOKAROUND_SPEED = 0.02
 
 func _enter_tree() -> void:
@@ -30,36 +33,37 @@ func _enter_tree() -> void:
 func _input(event: InputEvent) -> void:
 	
 	if event is InputEventMouseMotion and event.button_mask & 1:
-		rot_x += event.relative.x * LOOKAROUND_SPEED
-		rot_y += event.relative.y * LOOKAROUND_SPEED
+		screen_x_rotate += event.relative.x * LOOKAROUND_SPEED
+		screen_y_rotate += event.relative.y * LOOKAROUND_SPEED
 		
 		transform.basis = Basis();
-		rotate_object_local(Vector3(0, -1, 0), rot_x)
-		#rotate_object_local(Vector3(-1, 0, 0), rot_y)
-		rotate_object_local(Vector3(0, 0, -1), rot_y)
+		rotate_object_local(Vector3(0, -1, 0), screen_x_rotate)
+		rotate_object_local(Vector3(0, 0, -1), screen_y_rotate)
+		return
+		
+		
+	if event.is_action("break"):
+		var new_velocity = linear_velocity
+		linear_velocity
+		linear_velocity = Vector3.ZERO
 		return
 	
 	#print("processing input")
 	if event.is_action("move_right"):
-		pending_impules.z +=1
+		pending_impules.z += 1
 	elif event.is_action("move_left"):
-		pending_impules.z -=1
+		pending_impules.z -= 1
 	elif event.is_action("move_back"):
-		pending_impules.x +=1
+		pending_impules.x += 1
 	elif event.is_action("move_forward"):
-		pending_impules.x +=1
+		pending_impules.x += 1
 	elif event.is_action("move_up"):
-		pending_impules.y +=1
+		pending_impules.y += 1
 	elif event.is_action("move_down"):
-		pending_impules.y -=1
-		
-	
-
+		pending_impules.y -= 1
 		
 	pending_impules = transform.basis * pending_impules
 	
-	#add_constant_central_force(pending_impules)
-	#add_force(pending_impules)
-	#add_constant_central_force(pending_impules)
-	constant_force += pending_impules
+	linear_velocity += pending_impules * acceleration_rate
+
 	pending_impules = Vector3.ZERO

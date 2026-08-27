7943f5a5afd234ebe382324f956b8e32714c36d7
richardjankovich6 <jedidallin@gmail.com>
2026-08-27 16:32:59 -0600

    make acceleration rate be applied relative to ship rotation

--- player/PlayerShip.gd
+++ player/PlayerShip.gd
@@ -39,25 +39,26 @@ func _input(event: InputEvent) -> void:
 		transform.basis = Basis();
 		rotate_object_local(Vector3(0, -1, 0), screen_x_rotate)
 		rotate_object_local(Vector3(0, 0, -1), screen_y_rotate)
-		#return
 		
 		
 	if event.is_action("break"):
 		
+		var current_velocity = transform.basis * linear_velocity
+		
 		if abs(linear_velocity.x) >= acceleration_rate.x:
-			pending_impules.x = -1 if _isPositive(linear_velocity.x) else 1
+			pending_impules.x = -1 if _isPositive(current_velocity.x) else 1
 		else:
 			linear_velocity.x = 0
 			pending_impules.x = 0
 			
 		if abs(linear_velocity.y) >= acceleration_rate.y:
-			pending_impules.y = -1 if _isPositive(linear_velocity.y) else 1
+			pending_impules.y = -1 if _isPositive(current_velocity.y) else 1
 		else:
 			linear_velocity.y = 0
 			pending_impules.y = 0
 			
 		if abs(linear_velocity.z) >= acceleration_rate.z:
-			pending_impules.z = -1 if _isPositive(linear_velocity.z) else 1
+			pending_impules.z = -1 if _isPositive(current_velocity.z) else 1
 		else:
 			linear_velocity.z = 0
 			pending_impules.z = 0
@@ -76,11 +77,13 @@ func _input(event: InputEvent) -> void:
 		elif event.is_action("move_down"):
 			pending_impules.y -= 1
 		
-	pending_impules = transform.basis * pending_impules
+	#pending_impules = pending_impules * acceleration_rate
+	#pending_impules = transform.basis * pending_impules
+	pending_impules = transform.basis * (pending_impules * acceleration_rate)
 	
-	linear_velocity += pending_impules * acceleration_rate
+	linear_velocity += pending_impules
 
 	pending_impules = Vector3.ZERO
 	
-func _isPositive(value: float) -> bool:
+func _isPositive(value: Variant) -> bool:
 	return value >= 0

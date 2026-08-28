990de1d5d2a22ef8189e4af5c5101fae3924e967
richardjankovich6 <jedidallin@gmail.com>
2026-08-28 12:31:31 -0600

    remove unused code and simplify break functionality

--- player/PlayerShip.gd
+++ player/PlayerShip.gd
@@ -1,13 +1,12 @@
 extends RigidBody3D
 
-var pending_impules : Vector3 = Vector3.ZERO
+#var pending_impules : Vector3 = Vector3.ZERO
 
 #var acceleration_rate : Vector3 = Vector3(1.0, 0.5, 0.4)
 var acceleration_rate : Vector3 = Vector3(10.0, 5, 4)
 
 var screen_x_rotate : float = 0.0
 var screen_y_rotate : float = 0.0
-var LOOKAROUND_SPEED = 0.002
 
 var rotation_speed = 0.02
 
@@ -15,22 +14,6 @@ func _enter_tree() -> void:
 	gravity_scale = 0
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-#func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	#pass
-	#if Input.is_action_pressed("move_right"):
-		#pending_impules.x +=1
-	#if Input.is_action_pressed("move_left"):
-		#pending_impules.x -=1
-	#if Input.is_action_pressed("move_back"):
-		#pending_impules.z +=1
-	#if Input.is_action_pressed("move_forward"):
-		#pending_impules.z -=1
-	#if Input.is_action_pressed("move_up"):
-		#pending_impules.y +=1
-	#if Input.is_action_pressed("move_down"):
-		#pending_impules.y -=1
-	#constant_force += pending_impules
-	#pending_impules = Vector3.ZERO
 
 func _process(delta: float) -> void:
 	
@@ -46,49 +29,41 @@ func _input(event: InputEvent) -> void:
 	
 	#var delta: float = get_process_delta_time()
 	
+	var pending_impules: Vector3 = Vector3.ZERO
+	
 	if event.is_action("quit"):
 		get_tree().quit()
-	
-	#if event is InputEventMouseMotion and event.button_mask & 1:
-		
-		#var center = get_viewport().get_visible_rect().size / 2
-		#
-		#var dx = event.position.x - center.x
-		#var dy = event.position.y - center.y
-		#if abs(dx) > 5:
-			#rotation.z -= dx * rotation_speed * delta / 10
-			#rotation.z = clamp(rotation.z, -360, 360)
-#
-		#if abs(dy) > 5:
-			#rotation.y -= dy * rotation_speed * delta
-			#rotation.y = clamp(rotation.y, -360, 360)
-		
-		#transform.basis = Basis();
-		#rotate_object_local(Vector3(0, -1, 0), event.relative.x * rotation_speed * delta)
-		#rotate_object_local(Vector3(0, 0, -1), event.relative.y * rotation_speed * delta)
-		
 		
 	if event.is_action("break"):
-		
 		var current_velocity = transform.basis * linear_velocity
+		#var current_velocity = linear_velocity
 		
-		if abs(linear_velocity.x) >= acceleration_rate.x:
-			pending_impules.x = -1 if _isPositive(current_velocity.x) else 1
-		else:
-			linear_velocity.x = 0
-			pending_impules.x = 0
-			
-		if abs(linear_velocity.y) >= acceleration_rate.y:
-			pending_impules.y = -1 if _isPositive(current_velocity.y) else 1
-		else:
-			linear_velocity.y = 0
-			pending_impules.y = 0
-			
-		if abs(linear_velocity.z) >= acceleration_rate.z:
-			pending_impules.z = -1 if _isPositive(current_velocity.z) else 1
-		else:
-			linear_velocity.z = 0
-			pending_impules.z = 0
+		#for i in range (linear_velocity.length() - 1):
+		for i in range (3):
+			#var curr: float = abs(linear_velocity[i])
+			if abs(linear_velocity[i]) >= acceleration_rate[i]:
+				pending_impules[i] = -1 if current_velocity[i] >= 0 else 1
+			else:
+				linear_velocity[i] = 0
+				pending_impules[i] = 0
+		
+		#if abs(linear_velocity.x) >= acceleration_rate.x:
+			#pending_impules.x = -1 if _isPositive(current_velocity.x) else 1
+		#else:
+			#linear_velocity.x = 0
+			#pending_impules.x = 0
+			#
+		#if abs(linear_velocity.y) >= acceleration_rate.y:
+			#pending_impules.y = -1 if _isPositive(current_velocity.y) else 1
+		#else:
+			#linear_velocity.y = 0
+			#pending_impules.y = 0
+			#
+		#if abs(linear_velocity.z) >= acceleration_rate.z:
+			#pending_impules.z = -1 if _isPositive(current_velocity.z) else 1
+		#else:
+			#linear_velocity.z = 0
+			#pending_impules.z = 0
 			
 	else:
 		if event.is_action("move_right"):
@@ -104,17 +79,10 @@ func _input(event: InputEvent) -> void:
 		elif event.is_action("move_down"):
 			pending_impules.y -= 1
 		
-	#pending_impules = pending_impules * acceleration_rate
-	#pending_impules = transform.basis * pending_impules
 	pending_impules = transform.basis * (pending_impules * acceleration_rate)
-	
-	#linear_velocity += pending_impules * delta * 10
 	linear_velocity += pending_impules
 
-	pending_impules = Vector3.ZERO
 	
-	#if get_parent() is 
-	#get_parent().transform = linear_velocity
 	
 	
 

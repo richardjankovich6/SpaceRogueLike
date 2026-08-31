babca2c4694edcab67a19aa5773a742a6e3715d3
richardjankovich6 <jedidallin@gmail.com>
2026-08-31 15:34:52 -0600

    add _handleThrustInput function to read thrust inputs on tick update _handleMouseInput to correctly update ship pitch and yaw

--- player/PlayerShip.gd
+++ player/PlayerShip.gd
@@ -3,12 +3,12 @@ extends RigidBody3D
 #var pending_impules : Vector3 = Vector3.ZERO
 
 #var acceleration_rate : Vector3 = Vector3(1.0, 0.5, 0.4)
-var acceleration_rate : Vector3 = Vector3(10.0, 5, 4)
+var acceleration_rate : Vector3 = Vector3(15.0, 8, 8)
 
 var screen_x_rotate : float = 0.0
 var screen_y_rotate : float = 0.0
 
-var rotation_speed = 0.02
+var rotation_speed = 0.005
 
 func _enter_tree() -> void:
 	gravity_scale = 0
@@ -19,6 +19,8 @@ func _process(delta: float) -> void:
 	
 	_handleMouseInput(delta)
 	
+	_handleThrustInput(delta)
+	
 	var parent: Node3D = get_parent_node_3d()
 	if parent != null:
 		parent.transform = parent.transform.translated(linear_velocity * delta)
@@ -26,6 +28,7 @@ func _process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	return
 	
 	var pending_impules: Vector3 = Vector3.ZERO
 	
@@ -58,24 +61,53 @@ func _input(event: InputEvent) -> void:
 	pending_impules = transform.basis * (pending_impules * acceleration_rate)
 	linear_velocity += pending_impules
 
+func _handleThrustInput(delta: float) -> void:
+	var pending_impules: Vector3 = Vector3.ZERO
+	
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+	
+	if Input.is_action_pressed("break"):
+		var current_velocity: Vector3 = transform.basis * linear_velocity
+		for i in range (3):
+			if abs(linear_velocity[i]) >= acceleration_rate[i]:
+				pending_impules[i] = -1 if current_velocity[i] >= 0 else 1
+			else:
+				linear_velocity[i] = 0
+				pending_impules[i] = 0
+			
+	else:
+		if Input.is_action_pressed("move_right"):
+			pending_impules.z += 1
+		elif Input.is_action_pressed("move_left"):
+			pending_impules.z -= 1
+		elif Input.is_action_pressed("move_backward"):
+			pending_impules.x -= 1
+		elif Input.is_action_pressed("move_forward"):
+			pending_impules.x += 1
+		elif Input.is_action_pressed("move_up"):
+			pending_impules.y += 1
+		elif Input.is_action_pressed("move_down"):
+			pending_impules.y -= 1
+		
+	pending_impules = transform.basis * (pending_impules * acceleration_rate)
+	#linear_velocity += pending_impules
+	linear_velocity += pending_impules * delta
+
 func _handleMouseInput(delta: float) -> void:
 	var mouse_velocity = Input.get_last_mouse_screen_velocity()
-	#var mouse_velocity = Input.get_last_mouse_velocity()
 	
-	var dx = mouse_velocity.x
-	var dy = mouse_velocity.y
+	# rotation has z=pitch, y=yaw, x=roll
+	
+	var dx: float = mouse_velocity.x
+	var dy: float = mouse_velocity.y
 
-	if abs(dx) > 0:
-		rotation.z -= dx * rotation_speed * delta / 10
+	if abs(dy) > 0: # adjust pitch
+		rotation.z -= dy * rotation_speed * delta / 10
 		rotation.z = clamp(rotation.z, -360, 360)
 
-	if abs(dy) > 0:
-		rotation.y += dy * rotation_speed * delta
+	if abs(dx) > 0: # adjust yaw
+		rotation.y -= dx * rotation_speed * delta
 		rotation.y = clamp(rotation.y, -360, 360)
-	rotation.x = 0
-	
-	
-#func _handleFlightInput(delta: float) -> void:
-	#Input.action_press("break")
-	
-	#Input.
+		
+	#rotation.x = 0

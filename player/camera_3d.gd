e2498a7f52fa4f0f63457859ecd452ce8ec96c0b
richardjankovich6 <jedidallin@gmail.com>
2026-09-08 12:50:08 -0600

    update camera location and fov to be calculated per physics tick based on boost

--- player/camera_3d.gd
+++ player/camera_3d.gd
@@ -1,8 +1,17 @@
 #extends Camera3D
 extends Node3D
 
+@onready var camera : Camera3D = $Camera3D
+
 var playerShip: RigidBody3D
-static var cameraOffset: Vector3 = Vector3(4, 1, 0)
+const cameraOffset: Vector3 = Vector3(-5, 1, 0)
+
+const cameraDistance: float = 5
+const boostedModifier: float = 1.25
+var currentDistance: float = 5
+var boostAlpha: float = 0
+
+const cameraFOV: float = 90
 
 var boost_active : bool = false
 func activate_boost() -> void:
@@ -12,7 +21,19 @@ func deactivate_boost() -> void:
 
 func _ready() -> void:
 	playerShip = get_parent_node_3d().get_child(0)
+	#cameraOffset = camera.get_position()
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	set_rotation(playerShip.rotation)
 	set_position(playerShip.position)
+	
+	if (boost_active):
+		boostAlpha = clamp(boostAlpha + delta, 0, 1)
+	else:
+		boostAlpha = clamp(boostAlpha - delta, 0, 1)
+		
+	camera.fov = lerp(cameraFOV, cameraFOV / boostedModifier, boostAlpha)
+	currentDistance = lerp(cameraDistance, cameraDistance * boostedModifier, boostAlpha)
+	
+	camera.set_position(cameraOffset.normalized() * currentDistance)
+	
